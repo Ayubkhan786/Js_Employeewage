471a89f23f2fdc4a0ea772d34ad1a124596caf21
your Name <mohamedabdulayubm@gmail.com>
2022-08-07 20:02:49 +0530

    [Ayub] Validated data Using Regex pattern

--- EmployeeWage/EmployeePayroll.js
+++ EmployeeWage/EmployeePayroll.js
@@ -1,7 +1,4 @@
 class EmployeeePayrollData {
-  id;
-  salary;
-
   constructor(...params) {
     this.id = params[0];
     this.name = params[1];
@@ -13,6 +10,12 @@ class EmployeeePayrollData {
     return this._gender;
   }
   set gender(gender) {
+    let genederRegex = RegExp("^[Male,Female]{1}");
+    if (genederRegex.test(gender)) {
+      this._gender = gender;
+    } else {
+      throw "Invalid";
+    }
     this._gender = gender;
   }
   get startDate() {
@@ -29,12 +32,34 @@ class EmployeeePayrollData {
     if (nameRegex.test(name)) this._name = name;
     else throw "Name is incorrect";
   }
+  get salary() {
+    return this._salary;
+  }
+  set salary(salary) {
+    let salaryValidate = RegExp("^[1-9]{1}[0-9]{1,6}$");
+    if (salaryValidate.test(salary)) {
+      this._salary = salary;
+    } else {
+      throw "Wrong input";
+    }
+  }
+  get id() {
+    return this._id;
+  }
+  set id(id) {
+    let idValidate = RegExp("^[1-90-9]{1,3}$");
+    if (idValidate.test(id)) {
+      this._id = id;
+    } else {
+      throw "Invalid ";
+    }
+  }
   toString() {
     const option = { year: "numeric", month: "long", day: "numeric" };
     const empDate =
       this.startDate === undefined
         ? "undefined"
-        : new Date().toLocaleDateString("en-US", option);
+        : new Date().toLocaleDateString("en-us", option);
     return (
       "id = " +
       this.id +
@@ -49,19 +74,21 @@ class EmployeeePayrollData {
     );
   }
 }
-let employeePayrolldata = new EmployeeePayrollData(
-  3,
-  "Ayub",
-  500000,
-  "Male",
-  new Date()
-);
+let employeePayrolldata = new EmployeeePayrollData(10, "Ayub", 5000000, "Male");
 console.log(employeePayrolldata.toString());
 let newEmployeePayrolldata = new EmployeeePayrollData(
   2,
-  "Khan",
-  500000,
-  "Male",
+  "Shree",
+  550000,
+  "Female",
   new Date()
 );
 console.log(newEmployeePayrolldata.toString());
+let newEmployeePayrolldataOne = new EmployeeePayrollData(
+  3,
+  "Khan",
+  600000,
+  "Male",
+  new Date()
+);
+console.log(newEmployeePayrolldataOne.toString());
