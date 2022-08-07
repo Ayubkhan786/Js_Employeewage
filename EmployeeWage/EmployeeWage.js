c954524efe7f0249a7e8358d7c2a80556c424406
your Name <mohamedabdulayubm@gmail.com>
2022-08-07 16:45:26 +0530

    [Ayub] Employee Working hours

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -4,19 +4,21 @@ const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 
-let emphrs = 0;
-let empcheck = Math.floor(Math.random() * 10) % 3;
-switch (empcheck) {
-  case IS_PART_TIME:
-    console.log("Employee is part time");
-    emphrs = PART_TIME_HRS;
-    break;
-  case IS_FULL_TIME:
-    console.log("Employee is full time");
-    emphrs = FULL_TIME_HRS;
-    break;
-  default:
-    emphrs = 0;
+function getWorkingHrs(empcheck) {
+  switch (empcheck) {
+    case IS_PART_TIME:
+      console.log("Employee is part time");
+      return PART_TIME_HRS;
+
+    case IS_FULL_TIME:
+      console.log("Employee is full time");
+      return FULL_TIME_HRS;
+    default:
+      return 0;
+  }
 }
+
+let empcheck = Math.floor(Math.random() * 10) % 3;
+let emphrs = getWorkingHrs(empcheck);
 let empwage = emphrs * WAGE_PER_HR;
 console.log("Empwage :" + empwage);
