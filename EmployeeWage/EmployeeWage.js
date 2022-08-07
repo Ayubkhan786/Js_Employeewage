117795501e5968bf5bd6e7c2f247a1d50ab80d05
your Name <mohamedabdulayubm@gmail.com>
2022-08-07 14:42:37 +0530

    [Ayub] Employee Wage

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -1,11 +1,22 @@
-const IS_ABSENT=0;
-let empcheck=Math.floor(Math.random()*10)%2;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
 
-if(empcheck==IS_ABSENT)
-{
-   console.log("Employee is Absent");
+let emphrs = 0;
+let empcheck = Math.floor(Math.random() * 10) % 3;
+switch (empcheck) {
+  case IS_PART_TIME:
+    console.log("Employee is part time");
+    emphrs = PART_TIME_HRS;
+    break;
+  case IS_FULL_TIME:
+    console.log("Employee is full time");
+    emphrs = FULL_TIME_HRS;
+    break;
+  default:
+    emphrs = 0;
 }
-else
-{
-   console.log("Employee is Present")
-}
+let empwage = emphrs * WAGE_PER_HR;
+console.log("Empwage :" + empwage);
